d44363fcfdc7f47c08f4b155f3b98b9a050f32d0
Harrison Leath <hleath@me.com>
2020-06-05 13:25:41 -0400

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import { Box, Grommet, Heading, Main } from "grommet";
+import NavBar from "./components/NavBar";
+import Header from "./components/Header";
+import Card from "./components/Card";
+import Profile from "./components/Profile";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <Grommet full background='light-1'>
+      <Box>
+        <Box margin='auto' width='75%' justify='center'>
+          <NavBar headerTextColor='#444444'></NavBar>
+        </Box>
+        <Main
+          pad={{
+            top: "medium",
+            bottom: "medium",
+            left: "large",
+            right: "large",
+          }}
+          margin='auto'
+          width='75%'
+          justify='center'
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Header />
+          <Box
+            margin={{
+              top: "medium",
+            }}
+          >
+            <Box>
+              <Heading
+                style={{ lineHeight: 1.1, fontSize: 40, color: "#444444" }}
+              >
+                Bio
+              </Heading>
+              <Profile></Profile>
+            </Box>
+            <Box>
+              <Heading
+                style={{ lineHeight: 1.1, fontSize: 40, color: "#444444" }}
+              >
+                Projects
+              </Heading>
+              <Card title='Jotify'></Card>
+              <Card title='Jotify Website'></Card>
+              <Card title='DashButton'></Card>
+              <Card title='CBS Johns Creek'></Card>
+            </Box>
+          </Box>
+        </Main>
+      </Box>
+    </Grommet>
   );
 }
 
